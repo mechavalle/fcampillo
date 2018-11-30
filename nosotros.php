e86d58417989d837600467c221e99d38e59387bd
cristinalg <cristina_13logan@hotmail.com>
2018-11-30 16:23:26 -0600

    nosotros

--- nosotros.php
+++ nosotros.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="en-US" class="scheme_original">
-
 <head>
-   <meta charset="UTF-8" />
+    <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="format-detection" content="telephone=no">
-    <title>About us – Wedding Planner</title>
+    <title>About us – Fernanda Campillo</title>
     <link rel="icon" type="image/x-icon" href="images/logotipo.png"/>
     <link property="stylesheet" rel='stylesheet' href='css/fontello/css/fontello.css' type='text/css' media='all' />
     <link property="stylesheet" rel='stylesheet' href='css/style.css' type='text/css' media='all' />
@@ -13,16 +12,17 @@
     <link property="stylesheet" rel='stylesheet' href='css/template.css' type='text/css' media='all' />
     <link property="stylesheet" rel='stylesheet' href='css/core.animation.css' type='text/css' media='all' />
     <link property="stylesheet" rel='stylesheet' href='js/vendor/magnific/magnific-popup.css' type='text/css' media='all' />
+    <link property="stylesheet" rel='stylesheet' href='js/vendor/swiper/swiper.css' type='text/css' media='all' />
     <link property="stylesheet" rel='stylesheet' href='js/vendor/woocommerce/assets/css/woocommerce-smallscreen.css' type='text/css' media='only screen and (max-width: 768px)' />
     <link property="stylesheet" rel='stylesheet' href='js/vendor/woocommerce/assets/css/woocommerce-layout.css' type='text/css' media='all' />
     <link property="stylesheet" rel='stylesheet' href='js/vendor/woocommerce/assets/css/woocommerce.css' type='text/css' media='all' />
     <link property="stylesheet" rel='stylesheet' href='js/vendor/woocommerce/assets/css/plugin.woocommerce.css' type='text/css' media='all' />
-    <link property="stylesheet" rel='stylesheet' href='css/responsive.css' type='text/css' media='all' /> 
+    <link property="stylesheet" rel='stylesheet' href='css/responsive.css' type='text/css' media='all' />
     <link rel="stylesheet" href="fw/css/font-awesome.css"></link>
-</head>    
+</head>
 
-<body class="single single-product woocommerce woocommerce-page">
-
+<body class="single single-team">
+    <!--.body_wrap -->
     <header class="top_panel_wrap top_panel_style_1 scheme_original">
         <?php  include("header.php"); ?>
         <?php  
@@ -31,54 +31,45 @@
         ?> 
     </header>
 
-    <div class="column_container column-1_1">
-        <div class="top_panel_title title_present">
-            <img src="images/flores.jpg" style="margin-bottom: 150px;">
-        </div> 
-    </div>     
-    <div class="page_content_wrap page_paddings_yes">
-        <div class="content_wrap">
-            <!--.content -->
-            <div class="content">
-                <!--.post_item .post_item_single -->
-                <article class="post_item post_item_single">
-                    <!--.post_content-->
-                    <div class="post_content">
-                        <section class="no-col-padding">
-                            <div class="container">
-                                <div class="columns_wrap">
-                                    <div class="column_container column-1_1">
-                                        <div class="column-inner">
-                                            <div class="m_wrapper">
-                                                <div class="sc_section sc_section_block margin_top_medium2 aligncenter">
-                                                    <div class="sc_section_inner">
-                                                        <div class="sc_section_content_wrap">
-                                                            <div class="woocommerce columns-2">
-                                                                <ul class="products">
-                                                                    <li class="product has-post-thumbnail first">
-                                                                        <div class="isotope_wrap">
-                                                                            <div class="isotope_item isotope_item_masonry">
-                                                                                <div class="post_featured">
-                                                                                    <div class="post_thumb" data-image="images/post-1.jpg" data-title="The First Day of the Rest of Their Lives">
-                                                                                        <a class="hover_icon hover_icon_link" href="blog-standart-post.html"><img class="post-image" src="images/post-1-370x284.jpg" style="width: 400px; height: 400px;"></a>
-                                                                                    </div>
-                                                                                </div>
-                                                                            </div>
-                                                                        </div>
-                                                                    </li>
-                                                                    <li class="product has-post-thumbnail last" style="text-align: left; margin-left: -150px;">
-                                                                        <h6 class="widget_title" style="color: #cab374; font-size: 20px; text-align: left;">Fernanda Campillo</h6>
-                                                                        <div class="m_wrapper">
-                                                                            <p">Hola soy Fer, Licenciada en Dirección de Empresas de Entretenimiento y certificada por el INIBEP en Wedding & Event Planner. Lo que más me caracteriza es que siempre estoy creando ideas nuevas, es por eso que este gran proyecto lo creé para ti de una forma muy especial y junto con mi equipo coordinaremos minuciosamente cada paso en la organización de tu boda o evento."</p>
-                                                                            <p>Somos una empresa dedicada 100% a ti y a través de cinco puntos clave (planeación, diseño, administración, coordinación y supervisión) todo aquello que te gusta, buscas e imaginas te aseguramos que lo encontrarás con nosotros. Hemos planificado y ejecutado eventos únicos, ¡y esperamos sean más! Nos sentimos increíblemente afortunados de ser parte de un momento tan especial en la vida de nuestras parejas y clientes, ¡y estamos ansiosos por ayudarlo con cada detalle especial!
-                                                                            <br>Siempre buscaremos la perfección en nuestro trabajo: no existe una boda bien, sólo existen bodas perfectas.</p">
-                                                                            <p>Misión
-                                                                            <br>Estamos dedicados a la creación y planificación de eventos con un alto nivel de profesionalismo, en donde nos aseguraremos de innovar para sorprender a todos y cada uno de nuestros clientes, cuidando que nuestros servicios se cumplan en tiempo y forma.</p>
-                                                                            <p>Visión
-                                                                            <br>Ser una empresa líder dentro de la industria de los eventos, siendo ampliamente reconocidos por nuestro profesionalismo, calidad, vocación y compromiso con los objetivos de nuestros clientes.</p>
-                                                                        </div>
-                                                                    </li>
-                                                                </ul>
+    <div class="top_panel_title title_present">
+        <div class="top_panel_title_inner title_present bg-breadcrumbs">
+            <div class="content_wrap">
+                <br>
+                <div class="breadcrumbs">
+                <br>
+                </div> 
+                <br> 
+            </div>
+        </div>
+    </div>  
+    <!-- .page_content_wrap-->             
+    <div class="page_content_wrap page_paddings_no">
+        <!--.content -->
+        <div class="content">
+            <!-- .post_item .post_item_single_team -->
+            <article class="post_item post_item_single_team">
+                <!--.post_content-->
+                <div class="post_content">
+                    <section class="no-col-padding">
+                        <div class="content_container">
+                            <div class="columns_wrap">
+                                <div class="column_container column-1_1">
+                                    <div class="column-inner">
+                                        <div class="m_wrapper">
+                                            <div class="columns_wrap sc_columns columns_nofluid sc_columns_count_7 sc_our-team-single-column-margin">
+                                                <div class="column-3_7 sc_column_item odd first">
+                                                    <figure class="sc_image  sc_image_shape_square"><img src="images/team1.jpg" alt="" /></figure>
+                                                    </div><div class="column-4_7 sc_column_item even">
+                                                        <h6 class="sc_title sc_title_regular margin_bottom_tiny sc_our-team-single-title-margin" style="color: #cab374; font-size: 20px; text-align: left;">Fernanda Campillo</h6>
+                                                        <div class="m_text_column m_content_element ">
+                                                            <div class="m_wrapper" style="color: #737373;">
+                                                                <p">Hola soy Fer, Licenciada en Dirección de Empresas de Entretenimiento y certificada por el INIBEP en Wedding & Event Planner. Lo que más me caracteriza es que siempre estoy creando ideas nuevas, es por eso que este gran proyecto lo creé para ti de una forma muy especial y junto con mi equipo coordinaremos minuciosamente cada paso en la organización de tu boda o evento."</p>
+                                                                <p>Somos una empresa dedicada 100% a ti y a través de cinco puntos clave (planeación, diseño, administración, coordinación y supervisión) todo aquello que te gusta, buscas e imaginas te aseguramos que lo encontrarás con nosotros. Hemos planificado y ejecutado eventos únicos, ¡y esperamos sean más! Nos sentimos increíblemente afortunados de ser parte de un momento tan especial en la vida de nuestras parejas y clientes, ¡y estamos ansiosos por ayudarlo con cada detalle especial!
+                                                                <br>Siempre buscaremos la perfección en nuestro trabajo: no existe una boda bien, sólo existen bodas perfectas.</p">
+                                                                <p>Misión
+                                                                <br>Estamos dedicados a la creación y planificación de eventos con un alto nivel de profesionalismo, en donde nos aseguraremos de innovar para sorprender a todos y cada uno de nuestros clientes, cuidando que nuestros servicios se cumplan en tiempo y forma.</p>
+                                                                <p>Visión
+                                                                <br>Ser una empresa líder dentro de la industria de los eventos, siendo ampliamente reconocidos por nuestro profesionalismo, calidad, vocación y compromiso con los objetivos de nuestros clientes.</p>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -87,24 +78,23 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>  
-                        </section>
-                    </div>
-                        <!-- end .post_content-->
-                </article>
-                    <!-- end .post_item .post_item_single -->
-            </div>
-            <!-- end .content -->
-        <!-- end .sidebar -->
+                            </div>
+                        </div>  
+                    </section>
+                </div>
+                <!-- end .post_content-->
+            </article>
+            <!-- end .post_item .post_item_single_team -->
         </div>
-        <!-- end .content_wrap -->
+        <!--end .content -->
     </div>
+
     <footer class="footer_wrap widget_area scheme_original">
         <?php require("footer.php"); ?>
     </footer>
 
     <a href="#" class="scroll_to_top icon-up" title="Scroll to top"></a>
-    
+ 
     <script type='text/javascript' src='js/vendor/jquery-3.1.1.js'></script>
     <script type='text/javascript' src='js/vendor/jquery-migrate.min.js'></script>
     <script type='text/javascript' src='js/vendor/photostack/modernizr.min.js'></script>
@@ -115,8 +105,8 @@
     <script type='text/javascript' src='js/custom/template.init.js'></script>
     <script type='text/javascript' src='js/custom/template.shortcodes.js'></script>
     <script type='text/javascript' src='js/vendor/magnific/jquery.magnific-popup.min.js'></script>
-    <script type='text/javascript' src='js/vendor/jquery.isotope.min.js'></script>
     <script type='text/javascript' src='js/vendor/core.messages/core.messages.js'></script>
-
+    <script type='text/javascript' src='js/vendor/swiper/swiper.js'></script>
+ 
 </body>
-</html>                       
+</html>                             
