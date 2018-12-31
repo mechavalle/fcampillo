5e20d8a2c21b606ea9e79d96837cc5dcbbf46cff
cristinalg <cristina_13logan@hotmail.com>
2018-12-31 13:05:31 -0600

    aviso

--- aviso.php
+++ aviso.php
@@ -41,27 +41,23 @@
         <div class="content" style="text-align: center; padding-top: 30px;">
             <h3>AVISO DE PRIVACIDAD</h3>
             <p style="color: #737373;">
-            De conformidad con lo establecido en la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, MEXOUT EVENTS, S.A.<br> 
-            DE C.V. (“MEXOUT EVENTS”) pone a su disposición el siguiente aviso de privacidad.<br>
+            De conformidad con lo establecido en la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, MEXOUT EVENTS, S.A.
+            DE C.V. (“MEXOUT EVENTS”) pone a su disposición el siguiente aviso de privacidad.
             </p>
             <p style="color: #737373;">
-            MEXOUT EVENTS, es responsable del uso y protección de sus datos personales, en este sentido y atendiendo las obligaciones legales<br>
-            establecidas en la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, a través de este instrumento se informa a los<br>
+            MEXOUT EVENTS, es responsable del uso y protección de sus datos personales, en este sentido y atendiendo las obligaciones legales
+            establecidas en la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, a través de este instrumento se informa a los
             titulares de los datos, la información que de ellos se recaba y los fines que se le darán a dicha información.
             </p>
             <p style="color: #737373;">
-            Además de lo anterior, informamos a usted que MEXOUT EVENTS, tiene su domicilio ubicado en:
-            </p>
-            <p style="color: #737373;">
-            Cerrada Hidalgo 11-1, colonia Rancho San Francisco, alcaldía Álvaro Obregón, código<br>
+            Además de lo anterior, informamos a usted que MEXOUT EVENTS, tiene su domicilio ubicado en:<br>
+            Cerrada Hidalgo 11-1, colonia Rancho San Francisco, alcaldía Álvaro Obregón, código
             postal 01807, Ciudad de México, México.
             </p>
             <p style="color: #737373;">
-            Los datos personales que recabamos de usted serán utilizados para las siguientes finalidades, las cuales son necesarias para concretar nuestra<br> 
-            relación con usted así como atender los servicios y/o pedidos que solicite:
-            </p>
-            <p style="color: #737373;">
-            Llevar a cabo actividad comercial Mercadoctecnia.<br>
+            Los datos personales que recabamos de usted serán utilizados para las siguientes finalidades, las cuales son necesarias para concretar nuestra 
+            relación con usted, así como atender los servicios y/o pedidos que solicite:<br>
+            Llevar a cabo actividad comercial Mercadotecnia.<br>
             Publicidad.<br>
             Prospección comercial.<br>
             Ventas.<br>
@@ -83,9 +79,9 @@
             Datos Bancarios.
             </p>
             <p style="color: #737373;">
-            Por otra parte, informamos a usted, que sus datos personales serán compartidos con las siguientes autoridades, empresas, organizaciones o<br> 
+            Por otra parte, informamos a usted, que sus datos personales serán compartidos con las siguientes autoridades, empresas, organizaciones o 
             personas distintas a nosotros:<br>
-            Empresas de Mensajería y/o Paqueteria.<br>
+            Empresas de Mensajería y/o Paquetería.<br>
             Empresas de Calidad de Servicio.
             <p style="color: #737373;">
             Su información será compartida exclusivamente para los fines que a continuación se mencionan:<br>
@@ -94,16 +90,18 @@
             Para la evaluación de calidad del servicio.
             </p>
             <p style="color: #737373;">
-            Usted tiene en todo momento el derecho a conocer qué datos personales tenemos de usted, para qué los utilizamos y las condiciones del uso<br> 
-            que les damos (Acceso). Asimismo, es su derecho solicitar la corrección de su información personal en caso de que esté desactualizada, sea<br>
-            inexacta o incompleta (Rectificación); de igual manera, tiene derecho a que su información se elimine de nuestros registros o bases de datos<br>
-            cuando considere que la misma no está siendo utilizada adecuadamente (Cancelación); así como también a oponerse al uso de sus datos<br>
+            Usted tiene en todo momento el derecho a conocer qué datos personales tenemos de usted, para qué los utilizamos y las condiciones del uso 
+            que les damos (Acceso). Así mismo, es su derecho solicitar la corrección de su información personal en caso de que esté desactualizada, sea
+            inexacta o incompleta (Rectificación); de igual manera, tiene derecho a que su información se elimine de nuestros registros o bases de datos
+            cuando considere que la misma no está siendo utilizada adecuadamente (Cancelación); así como también a oponerse al uso de sus datos
             personales para fines específicos (Oposición). Estos derechos se conocen como derechos ARCO.
             </p>
             <p style="color: #737373;">
             Para el ejercicio de cualquiera de los derechos ARCO, se deberá presentar la solicitud respectiva a través del siguiente correo electrónico:<br>
-            INFO@MEXOUTEVENTS.COM<br>
-            Lo anterior también servirá para conocer el procedimiento y requisitos para el ejercicio de los derechos ARCO, no obstante, la solicitud de<br> 
+            INFO@MEXOUTEVENTS.COM
+            </p>
+            <p style="color: #737373;">
+            Lo anterior también servirá para conocer el procedimiento y requisitos para el ejercicio de los derechos ARCO, no obstante, la solicitud de 
             ejercicio de estos derechos debe contener la siguiente información:<br>
             Nombre.<br>
             Teléfono.<br>
@@ -112,8 +110,10 @@
             </p>
             <p style="color: #737373;">
             La respuesta a la solicitud se dará en 5 (cinco) días hábiles y se comunicará de la siguiente manera:<br>
-            A través del correo electrónico y/o por teléfono que se indique en la solicitud.<br>
-            Los datos de contacto de la persona o departamento de datos personales, que está a cargo de dar trámite a las solicitudes de derechos ARCO,<br> 
+            A través del correo electrónico y/o por teléfono que se indique en la solicitud.
+            </p>
+            <p style="color: #737373;">
+            Los datos de contacto de la persona o departamento de datos personales, que está a cargo de dar trámite a las solicitudes de derechos ARCO, 
             son los siguientes:<br>
             a) Nombre del responsable: Atención a Clientes MEXOUT EVENTS<br>
             b) Domicilio: Cerrada Hidalgo 11-1, colonia Rancho San Francisco, alcaldía Álvaro Obregón, código postal 01807, Ciudad de México, México<br>
@@ -122,20 +122,20 @@
             e) Otro medio de contacto: www.fernandacampillo.com
             </p>
             <p style="color: #737373;">
-            Cabe mencionar, que en cualquier momento usted puede revocar su consentimiento para el uso de sus datos personales. Del mismo modo,<br>
-            usted puede revocar el consentimiento que, en su caso, nos haya otorgado para el tratamiento de sus datos personales. Sin embargo, es<br> 
-            importante que tenga en cuenta que no en todos los casos podremos atender su solicitud o concluir el uso de forma inmediata, ya que<br>
-            es posible que por alguna obligación legal se requiera seguir tratando sus datos personales. Asimismo, usted deberá considerar que<br>
-            para ciertos fines, la revocación de su consentimiento implicará que no podamos seguir prestando el servicio que nos solicitó, o<br>
+            Cabe mencionar, que en cualquier momento usted puede revocar su consentimiento para el uso de sus datos personales. Del mismo modo,
+            usted puede revocar el consentimiento que, en su caso, nos haya otorgado para el tratamiento de sus datos personales. Sin embargo, es 
+            importante que tenga en cuenta que no en todos los casos podremos atender su solicitud o concluir el uso de forma inmediata, ya que
+            es posible que por alguna obligación legal se requiera seguir tratando sus datos personales. Así mismo, usted deberá considerar que,
+            para ciertos fines, la revocación de su consentimiento implicará que no podamos seguir prestando el servicio que nos solicitó, o
             la conclusión de su relación con nosotros.
             </p>
             <p style="color: #737373;">
-            Para revocar el consentimiento que usted otorga en este acto o para limitar su divulgación, se deberá presentar la solicitud respectiva<br>
+            Para revocar el consentimiento que usted otorga en este acto o para limitar su divulgación, se deberá presentar la solicitud respectiva
             a través del siguiente correo electrónico:<br>
             INFO@MEXOUTEVENTS.COM
             </p>
             <p style="color: #737373;">
-            Del mismo modo, podrá solicitar la información para conocer el procedimiento y requisitos para la revocación del consentimiento, así como<br>
+            Del mismo modo, podrá solicitar la información para conocer el procedimiento y requisitos para la revocación del consentimiento, así como
             limitar el uso y divulgación de su información personal, sin embargo, estas solicitudes deberán contener la siguiente información:<br>
             Nombre.<br>
             Teléfono.<br>
@@ -143,17 +143,20 @@
             Motivo de la Solicitud.
             </p>
             <p style="color: #737373;">
-            La respuesta a la solicitud de revocación o limitación de divulgación de sus datos se dará a más tardar en 5 (cinco) días hábiles y se<br>
+            La respuesta a la solicitud de revocación o limitación de divulgación de sus datos se dará a más tardar en 5 (cinco) días hábiles y se
             comunicará de la siguiente forma:<br>
             A través del correo electrónico y/o teléfono que se indique en la solicitud.
             </p>
             <p style="color: #737373;">
-            El presente aviso de privacidad puede sufrir modificaciones, cambios o actualizaciones derivadas de nuevos requerimientos legales; de nuestras<br>
-            propias necesidades por los productos o servicios que ofrecemos; de nuestras prácticas de privacidad; de cambios en nuestro modelo de negocio,<br>
-            o por otras causas, por lo cual, nos comprometemos a mantenerlo informado sobre los cambios que pueda sufrir el presente aviso de privacidad,<br> 
+            El presente aviso de privacidad puede sufrir modificaciones, cambios o actualizaciones derivadas de nuevos requerimientos legales; de nuestras
+            propias necesidades por los productos o servicios que ofrecemos; de nuestras prácticas de privacidad; de cambios en nuestro modelo de negocio,
+            o por otras causas, por lo cual, nos comprometemos a mantenerlo informado sobre los cambios que pueda sufrir el presente aviso de privacidad, 
             sin embargo, usted puede solicitar información sobre si el mismo ha sufrido algún cambio a través de la siguiente dirección electrónica:<br>
-            www.fernandacampillo.com<br><br><br>
-            ________________________________________<br>
+            www.fernandacampillo.com
+            </p>
+            <br>
+            <p style="color: #737373;">
+            _________________________________________________<br>
             Nombre y firma del titular de los datos personales
             </p>
             <p style="color: #737373;">
